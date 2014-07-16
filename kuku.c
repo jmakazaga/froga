ab368091b13cc00399d7d6e874d124c91fe98a15
Joseba Makazaga <joseba.makazaga@ehu.es>
2014-07-16 16:52:55 +0200

    berriako bigarrena

--- kuku.c
+++ kuku.c
@@ -6,7 +6,7 @@ char buf[80];
 
 printf("idatzi zerbait\n");
 scanf("%s",&(buf[0]));
-printf("Esandakoa(berria1):\n");
+printf("Esandakoa(berria1, bigarren commita):\n");
 printf(" %s\nda\n",buf);
 }
 
